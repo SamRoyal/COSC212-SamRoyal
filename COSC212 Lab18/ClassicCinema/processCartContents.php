63c6a88b93f248babc003e85ae2a3e0bb715b70b
SamRoyal <roysa235@student.otago.ac.nz>
2021-10-13 16:01:17 +1300

    Finished lab 18

---
+++ COSC212 Lab18/ClassicCinema/processCartContents.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+?>
+
+<table>
+
+<?php
+$arr = json_decode(file_get_contents("php://input"));
+foreach ($arr as $value){
+    echo '<tr><th>';
+    echo $value->title;
+    echo '</th><th>';
+    echo $value->price;
+    echo '</th></tr>';
+}
+?>
+</table>
+
+<?php
+
+$orders = simplexml_load_file('orders.xml');
+$newOrder = $orders->addChild('order');
+$delivery = $newOrder->addChild('delivery');
+
+
+$delivery->addChild('deliveryName', $_SESSION['deliveryName']);
+$delivery->addChild('deliveryEmail', $_SESSION['deliveryEmail']);
+$delivery->addChild('deliveryAddress1', $_SESSION['deliveryAddress1']);
+$delivery->addChild('deliveryAddress2', $_SESSION['deliveryAddress2']);
+$delivery->addChild('deliveryCity', $_SESSION['deliveryCity']);
+$delivery->addChild('deliveryPostcode', $_SESSION['deliveryPostcode']);
+
+$items = $newOrder->addChild('items');
+foreach ($arr as $value){
+    $item = $items->addChild('item');
+    $item->addChild('title',$value->title);
+    $item->addChild('price',$value->price);
+
+}
+
+$orders->saveXML('orders.xml');
+
+echo'<script type="text/javascript">
+          localStorage.clear();
+           </script>';
+
+$_SESSION = array();
+session_destroy();
+
+?>
---
+++ COSC212 Lab18/ClassicCinema/validateCheckout.php
@@ -0,0 +1,86 @@
+<?php
+session_start();
+$_SESSION['test'] = 'TEST';
+$_SESSION['deliveryName'] = $_POST['deliveryName'];
+$_SESSION['deliveryEmail'] = $_POST['deliveryEmail'];
+$_SESSION['deliveryAddress1'] = $_POST['deliveryAddress1'];
+$_SESSION['deliveryAddress2'] = $_POST['deliveryAddress2'];
+$_SESSION['deliveryCity'] = $_POST['deliveryCity'];
+$_SESSION['deliveryPostcode'] = $_POST['deliveryPostcode'];
+$_SESSION['cardNumber'] = $_POST['cardNumber'];
+$_SESSION['cardValidation'] = $_POST['cardValidation'];
+$_SESSION['cardType'] = $_POST['cardType'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Classic Cinema</title>
+        <meta charset="utf-8">
+        <link rel = "stylesheet" href="style.css">
+
+    </head>
+    <?php
+    $scriptList = array('jQuery-1.11.3.min.js');
+    include("header.php");
+    include("validationFunctions.php");
+    ?>
+    <body>
+<main>
+    <p id = "table">
+
+    </p>
+    <?php
+    $formOk = false;
+    if(isset($_POST['submit'])){
+        $formOk = true;
+
+        if(isEmpty($_POST['deliveryName'])){
+            $formOk = false;
+            echo "<p>you must enter a delivery name</p>";
+        }
+
+        if(isEmpty($_POST['deliveryEmail']) || !isEmail($_POST['deliveryEmail']) ){
+            $formOk = false;
+            echo "<p>you must enter a valid email address</p>";
+        }
+
+        if(isEmpty($_POST['deliveryAddress1'])){
+            $formOk = false;
+            echo "<p>you must enter a city</p>";
+        }
+        if (isEmpty($_POST['deliveryPostcode']) || (!isDigits($_POST['deliveryPostcode']) || !checkLength($_POST['deliveryPostcode'], 4))){
+            $formOk = false;
+            echo "<p>You must enter a valid postcode</p>";
+        }
+
+        if((isEmpty($_POST['cardNumber'])) || (!checkCardNumber($_POST['cardType'], $_POST['cardNumber']))){
+            $formOk = false;
+            echo "<p>You must enter a valid card number</p>";
+        }
+
+        if(!checkCardDate($_POST['cardMonth'],$_POST['cardYear'])) {
+            $formOk = false;
+            echo "<p>You must enter a valid card date</p>";
+
+        }
+        if (!checkCardVerification($_POST['cardType'], $_POST['cardValidation'])) {
+            $formOk = false;
+            echo "<p>Please enter a vaild CVC</p>";
+        }
+
+//        var_dump($_SESSION);
+
+
+
+    if($formOk == true){
+        echo "<script src='GetCartContents.js'></script>";
+
+
+    }
+
+  }
+    ?>
+
+</main>
+<?php include("footer.php"); ?>
+</body>
